ba06e1aecbbc05504ba15ee8599b5544b5803822
Cory Knox <me@coryknox.dev>
2026-06-29 18:30:01 -0700

    Add zapp

---
+++ zappUpdate.ps1
@@ -0,0 +1,46 @@
+$ErrorActionPreference = 'Stop'
+
+$chocoPackage = 'zapp'
+$chocoSource = 'https://community.chocolatey.org/api/v2/'
+$GitHubUser = "zsa"
+$GitHubRepo = "zapp"
+$AssetPattern = "zapp-windows-x86_64.zip"
+
+$Latest = Invoke-RestMethod "https://api.github.com/repos/$GitHubUser/$GitHubRepo/releases/latest"
+# $Current = choco search $chocoPackage --exact -r --source $chocoSource | ConvertFrom-Csv -Delimiter '|' -Header 'Name', 'Version'
+
+$latestVersion = [version]($Latest.tag_name -replace 'v', '')
+
+if ($null -eq $Current) {
+    $Current = [pscustomobject]@{Version = '0.0.0' }
+}
+if ([version]($Current.Version) -lt $latestVersion) {
+    $webClient = [System.Net.WebClient]::new()
+    $toolsDir = Join-Path $PSScriptRoot "packages\$chocoPackage"
+    $asset = $latest.assets | Where-Object name -Match $AssetPattern
+
+    $nuspec = Get-ChildItem $toolsDir -Recurse -Filter '*.nuspec' | Select-Object -ExpandProperty FullName
+    $install = Get-ChildItem $toolsDir -Recurse -Filter 'chocolateyinstall.ps1' | Select-Object -ExpandProperty FullName
+    $replacements = @(
+        @{
+            toReplace   = '[[VERSION]]'
+            replaceWith = $latestVersion
+            file        = $nuspec
+        }
+        @{
+            toReplace = '[[URL]]'
+            replaceWith = $asset.browser_download_url
+            file = $install
+        }
+        @{
+            toReplace = '[[CHECKSUM]]'
+            replaceWith = $asset.digest.Replace('sha256:','').ToUpper()
+            file = $install
+        }
+    )
+    foreach ($currReplacement in $replacements) {
+        (Get-Content $currReplacement.file).Replace($currReplacement.toReplace, $currReplacement.replaceWith) | Set-Content $currReplacement.file
+    }
+
+    choco pack $nuspec --output-directory "'$PSScriptRoot\updatedPackages'"
+}
