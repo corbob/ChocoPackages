f518733ae3fa61b8325c0c965579f9f5b602f6ee
Cory Knox <me@coryknox.dev>
2026-07-03 12:29:07 -0700

    Add devproxy

---
+++ packages/devproxy/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64 = "[[URL]]"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url64bit      = $url64
+  checksum64    = '[[CHECKSUM]]'
+  checksumType64= 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
